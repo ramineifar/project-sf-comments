3eeb44f25a36073a54e3ba1e6eee1596ecd793fe
rnd <rami_neifar@hotmail.fr>
2022-10-05 22:12:19 +0200

    Add form type for entity

---
+++ web/comments/src/Form/CommentType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('contenu', null, [
+                'label' => "Votre commentaire"
+            ])
+            ->add('author', null, [
+                'label' => "Votre nom (pseudo)"
+            ])
+            ->add('condition', CheckboxType::class, [
+                'mapped' => false,
+                'label' => "Accepter les conditions"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
